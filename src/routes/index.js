503f9e880c14b9dc9b26cbba56c35c847e12c790
Adriano Heissig <91757217+adrianorosset@users.noreply.github.com>
2023-02-28 08:08:57 -0300

    Criando outras formas de navegacao

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,13 +3,17 @@ import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
-import Stack from './Stack';
+// import Stack from './Stack';
+// import Tab from './Tab';
+import Drawer from './Drawer';
 
 export default props => {
   return (
     <SafeAreaView style={styles.container}>
       <NavigationContainer>
-        <Stack />
+        {/* <Stack /> */}
+        {/* <Tab /> */}
+        <Drawer />
       </NavigationContainer>
     </SafeAreaView>
   );
